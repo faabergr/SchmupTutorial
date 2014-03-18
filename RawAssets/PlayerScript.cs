e66dd998e5a938b99dfb3105cb5e5d1d8942ebfc
rfaaberg <rfaaberg@blizzard.com>
2014-03-18 12:40:18 -0700

    Formatting PlayerScript

--- RawAssets/PlayerScript.cs
+++ RawAssets/PlayerScript.cs
@@ -1,28 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
+    public Vector2 Speed = new Vector2(50, 50);
+    private Vector2 _movement;
 
-	public Vector2 Speed = new Vector2(50, 50);
+    // Use this for initialization
+    void Start()
+    {
 
-	private Vector2 movement;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
 
-	// Use this for initialization
-	void Start () {
+        _movement = new Vector2(inputX * Speed.x, inputY * Speed.y);
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float inputX = Input.GetAxis ("Horizontal");
-		float inputY = Input.GetAxis ("Vertical");
-		
-		movement = new Vector2 (inputX * Speed.x, inputY * Speed.y);	
-	}
-
-	void FixedUpdate() {
-		if (rigidbody2D  != null)
-			rigidbody2D.velocity = movement;
-	}
+    void FixedUpdate()
+    {
+        if (rigidbody2D != null)
+            rigidbody2D.velocity = _movement;
+    }
 }

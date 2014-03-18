22bf66b98cda8c5c3b4d625416d114714b8ed41b
Rick Faaberg <rick@faaberg.org>
2014-03-17 22:12:38 -0700

    Adding PlayerScript to RawAssets

---
+++ RawAssets/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 Speed = new Vector2(50, 50);
+
+	private Vector2 movement;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		
+		movement = new Vector2 (inputX * Speed.x, inputY * Speed.y);	
+	}
+
+	void FixedUpdate() {
+		if (rigidbody2D  != null)
+			rigidbody2D.velocity = movement;
+	}
+}

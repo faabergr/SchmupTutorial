a7f03a8610caad393560b78c4dcfa3ad3b9b1819
Rick Faaberg <rick@faaberg.org>
2014-03-19 01:22:46 -0700

    Adding scripts

---
+++ RawAssets/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+    private WeaponScript _weapon;
+
+    void Awake()
+    {
+        _weapon = GetComponent<WeaponScript>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_weapon != null && _weapon.CanAttack)
+        {
+            _weapon.Attack(true);
+        }
+	}
+}

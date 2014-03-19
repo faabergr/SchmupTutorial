5e464dcd8052936de40f53d4f09efaaf031415b1
Rick Faaberg <rick@faaberg.org>
2014-03-18 23:39:08 -0700

    Adding HealthScript, ShotScript, renamed octopus

---
+++ RawAssets/HealthScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+    /// <summary>
+    /// Total health
+    /// </summary>
+    public int HitPoints = 1;
+
+    /// <summary>
+    /// If this script is attached to an enemy
+    /// </summary>
+    public bool IsEnemy = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// Inflicts damage and determines if the game object should be destroyed
+    /// </summary>
+    /// <param name="damageCount"></param>
+    public void Damage(int damageCount)
+    {
+        HitPoints -= damageCount;
+
+        if (HitPoints <= 0)
+        {
+            // Dead!
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a shot?
+        ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+            // Don't shoot yourself
+            if (shot.IsEnemyShot != IsEnemy)
+            {
+                Damage(shot.Damage);
+
+                // Destroy shot gameobject, not shot script
+                Destroy(shot.gameObject);
+            }
+        }
+    }
+}
---
+++ RawAssets/ShotScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour
+{
+    /// <summary>
+    /// Damage inflicted
+    /// </summary>
+    public int Damage = 1;
+
+    /// <summary>
+    /// Was this shot by the player or an enemy?
+    /// </summary>
+    public bool IsEnemyShot = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this, 20); // destroy after 20 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
